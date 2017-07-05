d1183b4df6c2e5234c9f0cdafcf058e64d4b1297
Helen Johnson <johnsonhelen31@gmail.com>
2017-07-05 13:12:44 +0100

    OOP work

--- src/CompanyStaff.js
+++ src/CompanyStaff.js
@@ -1,12 +1,13 @@
-function CompanyStaff(staffID, staffName, staffLevel, dateJoined) {
+class CompanyStaff {
+  constructor(staffID, staffName, staffLevel, dateJoined){
 	this.staffID = staffID;
 	this.staffName = staffName;
 	this.staffLevel = staffLevel;
 	this.dateJoined = dateJoined;
-}
-	CompanyStaff.prototype = {
-    constructor: CompanyStaff,
-	levelSalary : function (level) {
+  }
+
+
+	levelSalary (level) {
 	switch (level) {
 		case "entry":
 			return 25000;
@@ -22,42 +23,42 @@ function CompanyStaff(staffID, staffName, staffLevel, dateJoined) {
       console.log('There is no Salary set for this level, please review');
       break;
 		}
-	},
+	}
 	
-	staffSalary : function(){
-	  base = this.baseSalary + this.levelSalary(this.staffLevel);
+	staffSalary (){
+	  let base = this.baseSalary + this.levelSalary(this.staffLevel);
 	return base;
-	},
+	}
 	
-	thirteenthMonth : function(){
-	  //return this.staffSalary() * 0.1;
-	  thirteenth = this.baseSalary + this.levelSalary(this.staffLevel);
+	thirteenthMonth (){
+	  let thirteenth = this.baseSalary + this.levelSalary(this.staffLevel);
 	  return thirteenth * 0.1;
-	},
+	}
 	
-	annualSalary : function(){
-    totalAnnual = this.thirteenthMonth() + this.staffSalary();
+	annualSalary() {
+    let totalAnnual = this.thirteenthMonth() + (this.staffSalary()*12);
     return totalAnnual;
   }
-};
+}
 
 //Create FulltimeStaff and ParttimeStaff classes inheriting from CompanyStaff
-function FulltimeStaff(){
-	CompanyStaff.apply(this, arguments);
+class FulltimeStaff extends CompanyStaff{
+	constructor(staffID, staffName, staffLevel, dateJoined){
+       super(staffID, staffName, staffLevel, dateJoined);
 	this.baseSalary = 150000;
+  }
+	
+
 }
 
-  FulltimeStaff.prototype = Object.create(CompanyStaff.prototype);
-
-function ParttimeStaff(){
-	CompanyStaff.apply(this, arguments);
+class ParttimeStaff extends CompanyStaff{
+	constructor(staffID, staffName, staffLevel, dateJoined){
+       super(staffID, staffName, staffLevel, dateJoined);
 	this.baseSalary = 20000;
 }
+}
 
-  ParttimeStaff.prototype = Object.create(CompanyStaff.prototype);
-
-//Create instances of the classes FulltimeStaff and ParttimeStaff
-var Helen = new FulltimeStaff('001', "Helen J", "Manager", "04/07/2017");
-var Rere = new ParttimeStaff('001', "Rejoice J", "Manager", "01/06/2017");
-var Tim = new FulltimeStaff('001', "Tim C", "Cleaner", "05/06/2017");
-
+  
+  module.exports.companyStaff = CompanyStaff;
+  module.exports.fulltimeStaff = FulltimeStaff;
+  module.exports.parttimeStaff = ParttimeStaff;
